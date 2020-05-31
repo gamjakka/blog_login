709553b0de3e410ba2a3199a435f7b75ceb96690
깃허브닉네임 <>
2020-06-01 02:47:01 +0900

    add login,out,register

--- account/urls.py
+++ account/urls.py
@@ -1,11 +1,11 @@
 
 from django.urls import path,include
-from django import .views   
+from . import views   
     
     
-      
     
 urlpatterns = [
-    path("/login", views.login_view, name="login"),
-    path("/logout", views.logout_view, name="logout"),
+    path("login/", views.login_view, name="login"),
+    path("logout/", views.logout_view, name="logout"),
+    path("register/", views.register_view, name="register"),
 ]
--- account/views.py
+++ account/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render, redirect
-from django.contrib.auth.forms. import AuthenticationForm
-from django.contrib.auth.forms. import UsercreationForm
+from .forms import LoginForm, RegisterForm
 from django.contrib.auth import login, authenticate, logout
 # Create your views here.
+
+
 def login_view(request):
     if request.method == "POST":
-        form = AuthenticationForm(request=request, data=request.POST)
+        form = LoginForm(request=request, data=request.POST)
         if form.is_valid():
             username = form.cleaned_data.get("username")
             password = form.cleaned_data.get("password")
@@ -16,9 +17,24 @@ def login_view(request):
                 return redirect("list")
 
     else:
-        form = AuthenticationForm()
+        form = LoginForm()
         return render(request, "account.html", {"form": form})
 
 def logout_view(request):
-    return redirect("list")
+    logout(request)
+    return redirect("login")
+
+def register_view(request):
+    if request.method == "POST":
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("list")
+        return render(request, "account.html",{"form": form})
+
+    else:
+        form = RegisterForm()
+        return render(request, "account.html", {"form": form})
 
